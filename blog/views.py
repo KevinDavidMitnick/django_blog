3d25eba211207043e9468fcd84f065a55e94e3e9
shadowalker <liucong911@gamail.com>
2017-08-09 13:10:55 +0000

    finish paginator

--- blog/views.py
+++ blog/views.py
@@ -3,14 +3,22 @@ from .models import Article,Category
 import markdown
 from comments.forms import CommentForm
 
+from django.core.paginator import Paginator,PageNotAnInteger,EmptyPage
+
 # Create your views here.
 def index(request,cid=-1):
     if cid == -1:
-        article_list = Article.objects.all().order_by('-create_time')[:8]
+        article_list = Article.objects.all().order_by('-create_time')
     else:
         category = get_object_or_404(Category,pk=cid)
-        article_list = Article.objects.filter(category=category).order_by('-create_time')[:8]
-    return render(request,'blog/index.html',context={'article_list':article_list})
+        article_list = Article.objects.filter(category=category).order_by('-create_time')
+    paginator = Paginator(article_list,3)
+    page = request.GET.get('page')
+    try:
+        pager = paginator.page(page)
+    except PageNotAnInteger:
+        pager = paginator.page(1)
+    return render(request,'blog/index.html',context={'pager':pager})
 
 def about(request):
     return render(request,'blog/about.html')
