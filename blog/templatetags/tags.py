4a4c50f04dd012cb2e06e23bd872f713030f96bc
shadowalker <liucong911@gamail.com>
2017-08-09 20:20:08 +0000

    finish tag

--- blog/templatetags/tags.py
+++ blog/templatetags/tags.py
@@ -1,8 +1,13 @@
 from django import template
-from ..models import Article,Category
+from django.db.models.aggregates import Count
+from ..models import Article,Category,Tag
 
 register = template.Library()
 
 @register.simple_tag
 def get_categories():
     return Category.objects.all()
+
+@register.simple_tag
+def get_tags():
+    return Tag.objects.annotate(num_articles=Count('article')).filter(num_articles__gt=0)
