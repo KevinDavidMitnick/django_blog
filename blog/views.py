38a7e3721a5a02d2e375152588a64e2c87ab2d71
liucong <liucong@chinac.com>
2017-08-10 18:50:42 +0800

    finish index.html

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from .models import Article
 
 # Create your views here.
 def index(request):
-    return render(request,'blog/index.html')
+    article_list = Article.objects.all().order_by('create_time')
+    return render(request,'blog/index.html',context={'article_list' : article_list})
 
 def about(request):
     return render(request,'blog/about.html')
